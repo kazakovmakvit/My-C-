2910e748bc7eb4cbd5cbb6fdc0bc352a3ea2b7a4
Maxim <kazakovmvmvm@gmail.com>
2022-07-10 16:38:27 +0300

    Задачи 3-4

--- Les_1/zadacha_3/Program.cs
+++ Les_1/zadacha_3/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Задача №3");
 
 
                                     /*Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
@@ -10,4 +10,15 @@ Console.WriteLine("Hello, World!");
 
                                     */
 
+Console.WriteLine("Введите число для проверки на четность");
+int a = int.Parse(Console.ReadLine());
 
+if( a % 2 == 0 )
+    {
+        Console.WriteLine( "Число a = " + a + ", Является четным");
+        
+    }
+else
+    {
+        Console.WriteLine( "Число a = " + a + ", Не является четным");
+    }
--- Les_1/zadacha_4/Program.cs
+++ Les_1/zadacha_4/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Задача №4");
+
+
+                                    /*
+
+                                    Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+                                    5 -> 2, 4
+                                    8 -> 2, 4, 6, 8
+
+                                    */
+
+
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine());
+
+
+for ( int i = 2; N >= i; i+=2)
+    {
+        Console.Write(" " + i);
+    }
