17c5a78c2e1b25ecaf88b5c467ca4619ade7dc68
Maxim <kazakovmvmvm@gmail.com>
2022-07-10 09:14:32 +0300

    Задачи первого урока 1-4

---
+++ Les_1/zadacha_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Введите число a");
+int a = int.Parse(Console.ReadLine());  /// int.Parse() переводит вводимые значения в числа (пользователь всегда вводит строку)
+
+Console.WriteLine("Введите число b");
+int b = int.Parse(Console.ReadLine());  /// int.Parse() переводит вводимые значения в числа (пользователь всегда вводит строку)
+if(a > b) 
+{
+    Console.WriteLine("Вывод: a больше b, а = " + a);
+}
+else
+{
+    Console.WriteLine("Вывод: a меньше b, b =  " + b);
+}
