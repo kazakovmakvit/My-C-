327ffb64b858ab2e487d6b7d9c1a0aacf1bb21c9
Maxim <kazakovmvmvm@gmail.com>
2022-07-19 18:02:36 +0300

    задачи 19, 21, 23

---
+++ Home_DZ-Lws-3/Zadacha 21/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Задача 21
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+double DlinnaOtrezka(int x, int y, int z, int x2, int y2, int z2)
+{
+    double Dlinna = Math.Sqrt((x2 - x) * (x2 - x) + (y2 - y) * (y2 - y) + (z2 - z) * (z2 - z));
+    return Dlinna;
+}
+
+Console.Write("Введите координаты X ");
+int x1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите координаты X_2 ");
+int x2 = int.Parse(Console.ReadLine());
+
+///
+
+Console.Write("Введите координаты Y ");
+int y1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите координаты Y_2 ");
+int y2 = int.Parse(Console.ReadLine());
+
+///
+
+Console.Write("Введите координаты Z ");
+int z1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите координаты Z_2 ");
+int z2 = int.Parse(Console.ReadLine());
+
+///
+
+Console.WriteLine("Отрезхок равен: " + DlinnaOtrezka(x1, y1, z1, x2, y2, z2));
+
+
---
+++ Home_DZ-Lws-3/Zadacha 23/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+
+Console.WriteLine("Введите число (N)");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Кубы чисел до N: ");
+for (int i = 1; i < num + 1; i++)
+{
+    int kubChisla = i * i * i;
+    Console.Write(" " + kubChisla);
+}
+
